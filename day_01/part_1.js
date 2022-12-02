31e39809eeb4d8f57daf631e9bb90a2e70f4aa1a
Admiral Potato <admiral@nuclearpixel.com>
2022-12-01 23:27:23 -0700

    Day 1, Part 1

---
+++ day_01/part_1.js
@@ -0,0 +1,20 @@
+const fs = require('node:fs');
+const input = fs.readFileSync('input.txt', 'utf-8').trim();
+
+const parseLine = (line) => parseInt(line, 10);
+const elfs = input.split('\n\n').map((lines) => lines
+  .split('\n')
+  .map(parseLine)
+);
+const scores = [];
+const getTotal = (numbers) => numbers.reduce((acc, value) => acc + value, 0);
+elfs.forEach((numbers) => {
+  const total = getTotal(numbers);
+  scores.push(total);
+});
+
+// console.log('scores', scores.join('\n'));
+
+scores.sort((a, b) => a - b);
+// console.log('scores sorted', scores.join('\n'));
+console.log('highest', scores.slice(-1).join());
